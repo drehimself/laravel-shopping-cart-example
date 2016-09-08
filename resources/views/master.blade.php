94bd1ee290adeccf4ee9f86a427b7a6707b9f17f
afolabi legunsen <aphoe@outlook.com>
2016-09-08 15:33:57 +0100

    Updated URLs using url() function
    Some links (href & action attributes) assumed that repo will always be run from root folder. Used Laravel's `url()` to update respective links

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -56,18 +56,18 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="/">Laravel Shopping Cart Example</a>
+              <a class="navbar-brand" href="{{ url('/') }}">Laravel Shopping Cart Example</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
-                <li class="{{ set_active('shop') }}"><a href="/">Home/Shop</a></li>
+                <li class="{{ set_active('shop') }}"><a href="{{ url('/') }}">Home/Shop</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
 
               </ul>
               <ul class="nav navbar-nav navbar-right">
-                <li class="{{ set_active('wishlist') }}"><a href="/wishlist">Wishlist ({{ Cart::instance('wishlist')->count(false) }})</a></li>
-                <li class="{{ set_active('cart') }}"><a href="/cart">Cart ({{ Cart::instance('main')->count(false) }})</a></li>
+                <li class="{{ set_active('wishlist') }}"><a href="{{ url('/wishlist') }}">Wishlist ({{ Cart::instance('wishlist')->count(false) }})</a></li>
+                <li class="{{ set_active('cart') }}"><a href="{{ url('/cart') }}">Cart ({{ Cart::instance('main')->count(false) }})</a></li>
               </ul>
             </div><!--/.nav-collapse -->
           </div>
